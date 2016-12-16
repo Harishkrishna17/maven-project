293ca209881f03c897b874b5750d62cc37b365e3
Harishkrishna17 <harishkrishna17@gmail.com>
2016-12-16 11:07:47 +0530

    Add files via upload

---
+++ Greeter.java
@@ -0,0 +1,7 @@
+package hello;
+
+public class Greeter {
+	public String sayHello() {
+		return "Hello world! harish is a very good boy and he is funky";
+	}
+}
---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+package hello;
+
+import org.joda.time.LocalTime;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+		LocalTime currentTime = new LocalTime();
+		System.out.println("==================");
+		System.out.println("\n The current local time is: " + currentTime);
+		Greeter greeter = new Greeter();
+		System.out.println("==================\n");
+		System.out.println(greeter.sayHello());
+		System.out.println("==================");
+		System.out.println("\n Hello INIT GUYS ");
+		System.out.println("==================");
+		System.out.println("Iam in banglore \n ");
+		System.out.println("==================");
+		System.out.println("Iam in india \n ");
+		System.out.println("==================");
+		
+	}
+}
